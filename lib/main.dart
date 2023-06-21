44bf7dff0cf6ca5da494e473692721ec047acfce
Dingaan <velly.dingaan17@gmail.com>
2023-06-21 14:32:48 +0200

    clean up the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_in_app_purchase_demo/screens/store.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,42 +14,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Flutter In App Purchase Demo"),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              TextButton(
-                  onPressed: () => {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: ((context) => const Store()),
-                          ),
-                        )
-                      },
-                  child: const Text(
-                    "Open Store",
-                    style: TextStyle(fontSize: 20),
-                  )),
-            ],
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Flutter In App Purchase Demo"),
           ),
-        ));
+          body: const Center()),
+    );
   }
 }
